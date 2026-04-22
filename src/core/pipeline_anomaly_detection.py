97c5a3b2f79f4a380cd8830b2381057df8b93b96
August Oksavik <142782293+AugOks@users.noreply.github.com>
2026-04-22 11:54:19 +0200

    bug/100 cache (#113)
    * Added cache clearing at the end of pipeline
    
    * Fixed cached images staying in RAM after finished execution

--- src/core/pipeline_anomaly_detection.py
+++ src/core/pipeline_anomaly_detection.py
@@ -1,10 +1,12 @@
+import gc
 import logging
+import tracemalloc
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
 from core.color_diff import check_difference_two_images
 from pathlib import Path
 import time
-from controller.image_cache_controller import load_two_image_arrays
+from controller.image_cache_controller import load_two_image_arrays, ImageCache
 import geopandas as gpd
 import numpy as np
 import core.water_detector as wd
@@ -159,7 +161,6 @@ def start_anomaly_analysis(sosi_gdf: gpd.GeoDataFrame, image_folder_path: Path, 
         stop_analysis_event: Event called over grpc to stop processing the analysis and send back current dataset.
     """
     try:
-
         image_count = len(sosi_gdf)
 
         t0 = time.perf_counter()
@@ -167,7 +168,7 @@ def start_anomaly_analysis(sosi_gdf: gpd.GeoDataFrame, image_folder_path: Path, 
         anomaly_sets = []
         config = Config()
         log = True
-        with ThreadPoolExecutor() as executor:
+        with ThreadPoolExecutor(max_workers=4) as executor:
             for i in range(image_count - 1):
                 # Stops analysis if the stop_analysis_event has been triggered. This is cross-thread
                 if stop_analysis_event is not None and stop_analysis_event.is_set():
@@ -189,7 +190,7 @@ def start_anomaly_analysis(sosi_gdf: gpd.GeoDataFrame, image_folder_path: Path, 
 
                 futures = {
                     executor.submit(start_artifact_detection_analysis, image1,
-                                    config.get("pipeline", "artifact_block_increment"), log): "artifact",
+                                    int(config.get("pipeline", "artifact_block_increment")), log): "artifact",
                     executor.submit(start_glare_detection_analysis, arr1, img1_path, log): "glare",
                 }
                 if sosi_gdf.iloc[i]["stripenummer"] == sosi_gdf.iloc[i + 1]["stripenummer"]:
@@ -219,6 +220,11 @@ def start_anomaly_analysis(sosi_gdf: gpd.GeoDataFrame, image_folder_path: Path, 
 
         print("Overall time:", time.perf_counter() - t0)
         print(f"Found {image_count} images in the GeoPackage.")
+        del arr1
+        del arr2
+        cache = ImageCache()
+        cache.clear()
+        gc.collect()
         return anomaly_sets
     except Exception as e:
         logger.error("Anomaly analysis failed, excpt_msg:%s",e,  extra={"analysis": "overall_anomaly_analysis", "img_id": "multiple"})
